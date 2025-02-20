6ffaaba0d9697a96b81797e31b00e076a50ef4ce
siba02 <sibaprasad.tofan@gmail.com>
2025-02-20 15:34:00 +0530

    update

---
+++ countsubsetSum.java
@@ -0,0 +1,39 @@
+public class countsubsetSum {
+    public static int sol(int[]nums,int sum){
+      
+        int n=nums.length;
+        int t[][] =new int [n+1][sum+1];
+
+        for (int i=0;i<=n;i++){
+            for(int j=0;j<=sum;j++){
+                if(i==0){
+                   t[i][j] = 0;
+                }
+                if(j==0){
+                   t[i][j] = 1;
+            }
+        }
+
+ }
+
+ for(int i=1;i<=n;i++){
+     for(int j=1;j<=sum;j++){
+         if(nums[i-1]<=j)
+            t[i][j]=t[i-1][j-nums[i-1]] + t[i-1][j];
+         else
+            t[i][j]=t[i-1][j];
+     }
+ }
+ return t[n][sum];
+}
+    public static void main(String[] args) {
+      int nums[]={2,3,5,8,10};
+      int sum=8;
+
+      
+      System.out.println("Number of subsets with sum 10: " + sol(nums,sum));
+        
+    }
+}
+    
+
--- subsetSum.java
+++ subsetSum.java
@@ -2,18 +2,30 @@ public class subsetSum {
     
 
         public static void main(String[] args){
-            int[] arr = {1, 2, 3, 4, 5};
-            int target = 30;
-            boolean[][] t = new boolean[arr.length + 1][target + 1];
+            int[] arr = {1, 2, 3, 4, 5,7};
+            int target = 10;
+            boolean [][] t = new boolean [arr.length + 1][target + 1];
     
             // Correct Initialization
-            for (int i = 0; i <= arr.length; i++) {
-                t[i][0] = true; 
+
+            for (int i = 0; i <= arr.length; i++){
+                for (int j = 0; j <= target; j++){
+                    if(i==0){
+                    t[i][j] = false;
+                    }
+                   if(j==0){
+                    t[i][j] = true;
+       
             }
+        }
+    }
+            // for (int i = 0; i <= arr.length; i++) {
+            //     t[i][0] = true; 
+            // }
     
-            for (int j = 1; j <= target; j++) {
-                t[0][j] = false; 
-            }
+            // for (int j = 0; j <= target; j++) {
+            //     t[0][j] = false; 
+            // }
     
             // Fill the DP table
             for (int i = 1; i <= arr.length; i++) {
