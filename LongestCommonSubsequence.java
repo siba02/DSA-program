37321e56c139bde70a8642c1661208a51cb1385c
siba02 <sibaprasad.tofan@gmail.com>
2025-03-06 23:19:13 +0530

    updates

---
+++ LongestCommonSubsequence.java
@@ -0,0 +1,33 @@
+public class LongestCommonSubsequence {
+    public static int Lcs(String x, String y,int m,int n) {
+        int t[][]= new int[m+1][n+1];
+        for (int i=0; i<=m; i++) {
+            for (int j=0; j<=n; j++) {
+                if (i == 0 || j == 0)
+                    t[i][j] = 0;
+                
+            }
+        }
+        for (int i=1; i<=m; i++) {
+            for (int j=1; j<=n; j++) {
+                if(x.charAt(i-1)==y.charAt(j-1)){
+                    t[i][j] = t[i-1][j-1] + 1;
+                }
+                else
+                t[i][j] = Math.max(t[i-1][j], t[i][j-1]);
+            }
+        }
+        return t[m][n];
+
+    }
+    public static void main(String[] args) {
+        String x = "abcde";
+        String y = "ace";
+        int m = x.length();
+        int n = y.length();
+        System.out.println("Length of LCS is " + Lcs(x, y, m, n));
+       
+        
+    }
+    
+}
